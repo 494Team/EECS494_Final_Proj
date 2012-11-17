73058c2c5a829ae51cd2765634b74486ad7bdec0
Guoxing Li <liguoxing64@gmail.com>
2012-11-17 13:49:23 -0500

    Change ##get_hit()## to ##dec_health()##

--- jni/application/Agent.h
+++ jni/application/Agent.h
@@ -3,6 +3,7 @@
 
 #include "Moving_object.h"
 #include "Sim_object.h"
+#include "Collision.h"
 
 namespace Flame {
 
@@ -13,23 +14,28 @@ namespace Flame {
       const float &speed_ = 0.0f,
       const float &radius_ = 0.0f,
       const Zeni::Point2f &location_ = Zeni::Point2f())
-    : health(health_), alive(true), Moving_object(speed_, location_), body(location_, radius_) {
-    }
+    : health(health_), initial_health(health_), alive(true), 
+      Moving_object(speed_, location_), body(location_, radius_)
+    {}
 
     Zeni::Point2f get_location() const {return Moving_object::get_current_location();}
     Flame::Collision_circle get_body() const {return body;}
     bool is_alive() const {return alive;}
     
-    // getting hit cause health going down
-    void get_hit(const float &damage) {
+    // decrease health (if damage is negative, then increase)
+    void dec_health(const float &damage) {
       health -= damage;
       if (health < 0) {
         alive = false;
         health = 0.0f;
       }
+      if (health > initial_health) {
+        health = initial_health;
+      }
     }
 
   private:
+    float initial_health;
     float health;
     bool alive;
     Flame::Collision_circle body;
